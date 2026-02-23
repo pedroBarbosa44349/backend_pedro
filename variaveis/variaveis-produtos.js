234deb4a2defaba21d08f70a0b263dd9253e66bf
pedroBarbosa44349 <pedro.lima.barbosa@escola.pr.gov.br>
2026-02-23 12:35:57 +0000

    Criar variáveis para Pessoa e Produto

---
+++ variaveis/variaveis-produtos.js
@@ -0,0 +1,13 @@
+/** 
+ * CRIE 5 VARIÁVEIS PARA CARACTERÍSTICAS DE UM PRODUTO DO MERCADO LIVRE
+ * CONSIDER SE VARIÁVEL ,É let ou const
+ * IMPRIMA COM console.log(<variavel>);
+ * IMPRIMA COM console.log("NOME-VARIAVEL: " + <variavel>)
+ */
+ let nomeProduto = "camisa"
+
+
+
+
+ console.timeLog(nomeProduto)
+ console.log("Nome: " + nomeProduto)
---
+++ variaveis/variavel-pessoas.js
@@ -0,0 +1,28 @@
+// , var, let, const (valores reservados da linguagem)
+// 0-9, 0-7, 0-1, 0-9,A,B,C,D,E,F
+
+let nome = 'pedro'            // string
+let sobrenome = 'Henrique'    // string
+const cpf = '123456789-90'    // string
+const rg = '123456 ssp'       // string   
+let idade = 22                // number
+let soleitro = true           // boolean
+let nota = 7 + 2              // number
+
+console.log(nome)
+console.log(sobrenome)
+console.log(cpf)
+console.log(rg)
+console.log(idade)
+console.log(soleitro)
+console.log(nota)
+
+
+console.log(typeof nome)
+console.log(typeof nome)
+console.log(typeof sobrenome)
+console.log(typeof cpf)
+console.log(typeof rg)
+console.log(typeof idade)
+console.log(typeof soleitro)
+console.log(typeof nota)
